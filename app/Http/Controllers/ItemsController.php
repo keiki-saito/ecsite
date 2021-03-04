51f2f8ce4f0307837001ea7baba1b68d678125a1
keiki-saito <72840475+keiki-saito@users.noreply.github.com>
2021-03-04 23:34:33 +0900

    Update ItemsController.php

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Item;
 use App\Review;
-use App\category;
+use App\Category;
 use App\Order;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
